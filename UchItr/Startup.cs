289d498572d321c0f1de0f868f41c5a50367313e
Вячеслав Гольдюк < >
2016-02-09 22:04:49 +0300

    Start

---
+++ UchItr/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UchItr.Startup))]
+namespace UchItr
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
